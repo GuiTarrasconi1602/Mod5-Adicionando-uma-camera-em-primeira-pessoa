5f5a0fbbadb0147efcc0e0ba8ffe5dacb1bdf6c2
Guilherme Tarrasconi <guitarras1602@gmail.com>
2026-05-03 00:29:28 -0300

    Tarefa - Adicionando Iluminacao

--- src/TriangleTex.cpp
+++ src/TriangleTex.cpp
@@ -36,29 +36,68 @@ const GLchar *vertexShaderSource = R"(
 #version 400
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec2 texCoord;
+layout (location = 2) in vec3 normal;
 
 out vec2 TexCoord;
+out vec3 FragPos;
+out vec3 Normal;
 
 uniform mat4 model;
 uniform mat4 projection;
 
 void main()
 {
-	gl_Position = projection * model * vec4(position, 1.0);
-	TexCoord = texCoord;
+    vec4 worldPos = model * vec4(position, 1.0);
+
+    FragPos = worldPos.xyz;
+    Normal = mat3(transpose(inverse(model))) * normal;
+
+    TexCoord = texCoord;
+    gl_Position = projection * worldPos;
 }
 )";
 
 const GLchar *fragmentShaderSource = R"(
 #version 400
+
 in vec2 TexCoord;
+in vec3 FragPos;
+in vec3 Normal;
+
 out vec4 color;
 
 uniform sampler2D texture1;
 
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+// coeficientes
+uniform vec3 Ka; // ambiente
+uniform vec3 Kd; // difuso
+uniform vec3 Ks; // especular
+uniform float shininess;
+
 void main()
 {
-	color = texture(texture1, TexCoord);
+    vec3 texColor = texture(texture1, TexCoord).rgb;
+
+    // AMBIENTE
+    vec3 ambient = Ka * texColor;
+
+    // DIFUSO
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = Kd * diff * texColor;
+
+    // ESPECULAR
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), shininess);
+    vec3 specular = Ks * spec;
+
+    vec3 result = ambient + diffuse + specular;
+    color = vec4(result, 1.0);
 }
 )";
 
@@ -106,8 +145,9 @@ string loadMTL(const string& path)
 }
 
 struct Vertex {
-	vec3 position;
-	vec2 texCoord;
+    vec3 position;
+    vec2 texCoord;
+    vec3 normal;
 };
 
 pair<vector<Vertex>, string> loadOBJ(const string& path)
@@ -116,9 +156,15 @@ pair<vector<Vertex>, string> loadOBJ(const string& path)
 	vector<vec3> positions;
 	vector<vec2> texcoords;
 	vector<Vertex> vertices;
+	vector<vec3> normals;
 
 	ifstream file(path);
 	string line;
+
+	if (!file.is_open()) {
+		cout << "ERRO: nao abriu OBJ -> " << path << endl;
+		return {{}, ""};
+	}
 
 	while (getline(file, line))
 {
@@ -138,6 +184,12 @@ pair<vector<Vertex>, string> loadOBJ(const string& path)
         ss >> u >> v;
         texcoords.push_back(vec2(u,v));
     }
+	else if (type == "vn")
+	{
+		float x, y, z;
+		ss >> x >> y >> z;
+		normals.push_back(vec3(x,y,z));
+	}
     else if (type == "f")
     {
         string v1, v2, v3;
@@ -146,33 +198,38 @@ pair<vector<Vertex>, string> loadOBJ(const string& path)
         vector<string> vs = {v1, v2, v3};
 
         for (auto &v : vs)
-        {
-            int vi = 0, ti = 0, ni = 0;
+		{
+			int vi = 0, ti = 0, ni = 0;
 
-            if (sscanf(v.c_str(), "%d/%d/%d", &vi, &ti, &ni) == 3)
-            {}
-            else if (sscanf(v.c_str(), "%d/%d", &vi, &ti) == 2)
-            {}
-            else if (sscanf(v.c_str(), "%d", &vi) == 1)
-            {
-                ti = 0;
-            }
+			if (sscanf(v.c_str(), "%d/%d/%d", &vi, &ti, &ni) == 3) {}
+			else if (sscanf(v.c_str(), "%d/%d", &vi, &ti) == 2) {}
+			else if (sscanf(v.c_str(), "%d", &vi) == 1) {
+				ti = 0;
+			}
 
-            Vertex vert;
+			Vertex vert;
 
-            vert.position = positions[vi - 1];
+			vert.position = positions[vi - 1];
 
-            if (ti > 0 && ti <= texcoords.size()){
+			if (ti > 0 && ti <= texcoords.size())
+			{
 				vec2 uv = texcoords[ti - 1];
 				uv.x = uv.x - floor(uv.x);
 				uv.y = uv.y - floor(uv.y);
 				vert.texCoord = uv;
-			}else{
+			}
+			else
+			{
 				vert.texCoord = vec2(0.0f, 0.0f);
 			}
 
-            vertices.push_back(vert);
-        }
+			if (ni > 0 && ni <= normals.size())
+				vert.normal = normals[ni - 1];
+			else
+				vert.normal = vec3(0.0f, 0.0f, 1.0f);
+
+			vertices.push_back(vert);
+		}
     }
     else if (type == "mtllib")
     {
@@ -206,14 +263,18 @@ GLuint setupGeometry(vector<Vertex>& vertices)
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, texCoord));
 	glEnableVertexAttribArray(1);
 
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, normal));
+	glEnableVertexAttribArray(2);
+
 	return VAO;
 }
 
 int main()
 {
 	glfwInit();
+	
 
-	GLFWwindow *window = glfwCreateWindow(800, 600, "Adicionando Texturas - Guilherme Tarrasconi", NULL, NULL);
+	GLFWwindow *window = glfwCreateWindow(800, 600, "Adicionando Iluminação - Guilherme Tarrasconi", NULL, NULL);
 	glfwMakeContextCurrent(window);
 	glfwSetKeyCallback(window, key_control);
 	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
@@ -222,8 +283,23 @@ int main()
 	GLuint shader = setupShader();
 	glUseProgram(shader);
 
-	auto result = loadOBJ("../assets/Modelos3D/Cube.obj");
+	glUniform3f(glGetUniformLocation(shader, "lightPos"), 2.0f, 2.0f, 2.0f);
+	glUniform3f(glGetUniformLocation(shader, "viewPos"), 0.0f, 0.0f, 3.0f);
+
+	glUniform3f(glGetUniformLocation(shader, "Ka"), 0.2f, 0.2f, 0.2f);
+	glUniform3f(glGetUniformLocation(shader, "Kd"), 0.7f, 0.7f, 0.7f);
+	glUniform3f(glGetUniformLocation(shader, "Ks"), 1.0f, 1.0f, 1.0f);
+	glUniform1f(glGetUniformLocation(shader, "shininess"), 32.0f);
+
+	auto result = loadOBJ("C:/Users/guita/Downloads/Mod-2-Instanciando-objetos-na-cena-3D-main (1)/Mod-2-Instanciando-objetos-na-cena-3D-main/assets/Modelos3D/Cube.obj");
+	
 	vector<Vertex> vertices = result.first;
+	cout << "Vertices carregados: " << vertices.size() << endl;
+
+	if (vertices.empty()) {
+		cout << "ERRO: OBJ nao carregado!" << endl;
+	}
+
 	string mtlFile = result.second;
 	GLuint VAO = setupGeometry(vertices);
 	cout << "Vertices carregados: " << vertices.size() << endl;
@@ -240,7 +316,12 @@ int main()
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 	
 	int width, height, nrChannels;	
-	unsigned char *data = stbi_load("../assets/Modelos3D/brick.png", &width, &height, &nrChannels, 0);
+	unsigned char *data = stbi_load("C:/Users/guita/Downloads/Mod-2-Instanciando-objetos-na-cena-3D-main (1)/Mod-2-Instanciando-objetos-na-cena-3D-main/assets/Modelos3D/brick.png", &width, &height, &nrChannels, 0);
+
+	if (!data)
+	{
+		cout << "ERRO: textura nao carregou\n";
+	}
 
 		if (!data)
 	{
